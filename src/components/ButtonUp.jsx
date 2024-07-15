250584c1526ae92958f5e1a973275dece51d9226
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-07-15 15:10:43 -0300

    Feat: Adicionado no Header o CV no lugar do 'inicio' e feito ajustes de estilo

--- src/components/ButtonUp.jsx
+++ src/components/ButtonUp.jsx
@@ -21,7 +21,7 @@ export function ButtonUp () {
     window.addEventListener('scroll', handleScroll);
 
     return (
-        <div id='button' className="hidden bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600 hover:opacity-95 p-1 md:p-2 w-12 h-12 md:w-14 md:h-14 rounded-full fixed bottom-4 right-6 md:right-8 md:bottom-7 lg:right-12 cursor-pointer shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" >
+        <div id='button' className="hidden bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600 hover:opacity-95 p-2 w-14 h-14 rounded-full fixed bottom-7 right-7 md:right-8 md:bottom-7 lg:right-12 cursor-pointer shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]" >
             <img src={up} alt="para cima" onClick={toggleScrollUp}/>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,17 +45,17 @@ export function Header() {
       <h1 className=" text-xl md:text-2xl font-black leading-snug text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600">Francine</h1>
 
       <div className="hidden md:flex font-semibold items-center dark:text-white">
-        <a className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('inicio')}</a>
-        <a className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('sobre')}</a>
-        <a className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('projetos')}</a>
+        <a href="#sobre" className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('sobre')}</a>
+        <a href="#projetos" className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('projetos')}</a>
+        <a target="_blank" rel="noopener noreferrer" href="/CV - Francine Cruz.docx" className="pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">CV</a>
 
         <button onClick={toggleDarkMode} className="mr-7 lg:mr-8 hover:opacity-70"><img src={isDarkMode ? light : dark} alt="dark/light mode" className="w-7" /></button>
 
         <button onClick={toggleLanguage} className="relative group"><img src={i18n.language === 'pt' ? en : br} alt={i18n.language === 'pt' ? 'en' : 'pt'} className="w-7 mr-7 lg:mr-8 hover:opacity-70"/><span className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-max max-w-xs bg-black text-white text-sm rounded px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{i18n.language === 'pt' ? 'English' : 'Português'}</span></button>
 
-        <button className="p-0.5 rounded-full from-indigo-400 via-pink-500 to-purple-500 bg-gradient-to-r">
+        <a href="#contato"><button className="p-0.5 rounded-full from-indigo-400 via-pink-500 to-purple-500 bg-gradient-to-r">
           <span className={`block px-4 py-2 font-semibold rounded-full bg-white text-black hover:bg-transparent hover:text-white transition dark:bg-[#1d1d1d] dark:text-white dark:hover:bg-transparent dark:hover:text-white`}>{t('contato')}</span>
-        </button>
+        </button></a>
 
       </div>
 
@@ -67,10 +67,10 @@ export function Header() {
       </div>
 
       {isMenuOpen && (
-        <div className="absolute top-16 bottom-72 left-0 w-full bg-white dark:bg-[#1d1d1d] flex flex-col items-center space-y-4 md:hidden font-semibold dark:text-white">
-          <a className="cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('inicio')}</a>
-          <a className="cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('sobre')}</a>
-          <a className="cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('projetos')}</a>
+        <div className="absolute top-16 pb-8 left-0 w-full bg-white dark:bg-[#1d1d1d] flex flex-col items-center space-y-4 md:hidden font-semibold dark:text-white">
+          <a href="#sobre" className="cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('sobre')}</a>
+          <a href="#projetos" className="cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">{t('projetos')}</a>
+          <a target="_blank" rel="noopener noreferrer" href="/CV - Francine Cruz.docx" className=" cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">CV</a>
 
           <div>
             <button onClick={toggleDarkMode} className="mr-7 hover:opacity-70"><img src={isDarkMode ? light : dark} alt="dark/light mode" className="w-7" /></button>
@@ -78,9 +78,9 @@ export function Header() {
             <button onClick={toggleLanguage} className="relative group"><img src={i18n.language === 'pt' ? en : br} alt={i18n.language === 'pt' ? 'en' : 'pt'} className="w-7 hover:opacity-70"/><span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max max-w-xs bg-black text-white text-sm rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{i18n.language === 'pt' ? 'English' : 'Português'}</span></button>
           </div>
 
-          <button className="p-0.5 rounded-full from-indigo-400 via-pink-500 to-purple-500 bg-gradient-to-r">
+          <a href="#contato"><button className="p-0.5 rounded-full from-indigo-400 via-pink-500 to-purple-500 bg-gradient-to-r">
             <span className={`block px-4 py-2 font-semibold rounded-full bg-white text-black hover:bg-transparent hover:text-white transition dark:bg-[#1d1d1d] dark:text-white dark:hover:bg-transparent dark:hover:text-white`}>{t('contato')}</span>
-          </button>
+          </button></a>
         </div>
       )}
 
