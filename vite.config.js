06d4981ee8ce1650d542bfb4ed2f05718eb0e7b6
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-07-16 11:55:19 -0300

    Chore: Configurando o Vite para permitir acesso no mobile para teste

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite o acesso a partir de qualquer endereço IP
+    port: 5173, // Porta em que o servidor rodará
+  },
 })
