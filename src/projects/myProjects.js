2baf23c57594a7b53a800d77a261b46a31c6aac6
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-20 17:58:30 -0300

    Feat: projeto full stack e modificado CV

--- src/projects/myProjects.js
+++ src/projects/myProjects.js
@@ -1,5 +1,6 @@
 import restaurante from './img/restaurante.png'
 import clima from './img/clima.png'
+import controlExpense from './img/controle-de-despesas.png'
 
 const rest = {
     name: "projects.restaurante_name",
@@ -31,7 +32,26 @@ const clim = {
     ]
 }
 
+const control = {
+    name: "projects.control_expense-name",
+    github: "https://github.com/Francine02/Controle-de-Despesas",
+    deploy: "https://controle-de-despesas-umber.vercel.app/auth/login",
+    img: controlExpense,
+    about: "projects.control_expense",
+    tech: [
+        'https://img.icons8.com/?size=100&id=NfbyHexzVEDk&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=20909&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=21278&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=108784&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=WbhlkucPF3tZ&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=HMAIkKPbzZIL&format=png&color=000000',
+        'https://img.icons8.com/?size=100&id=A3Ulk2RcONKs&format=png&color=000000'
+    ]
+}
+
 export const myProject = [
     rest,
-    clim
+    clim,
+    control
 ]
