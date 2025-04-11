ac3b2f157994544ce34f464c6eacf68541d97064
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-11 20:26:45 -0300

    refactor: separated into a reusable component to use in page navigation

---
+++ src/components/Header/Link.jsx
@@ -0,0 +1,11 @@
+export function Link({link, text, rel}) {
+    return (
+        <a
+            href={link}
+            rel={rel}
+            className="text-base md:text-lg md:pr-7 lg:pr-10 cursor-pointer hover:leading-snug hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-indigo-600 hover:via-pink-600 hover:to-purple-600">
+            {text}
+        </a>
+
+    )
+}
