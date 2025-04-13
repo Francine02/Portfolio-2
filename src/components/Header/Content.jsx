a9a76e95373d9e8c886ad88f473683b330870748
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-13 17:12:04 -0300

    Feat: props target to open new cv tab

--- src/components/Header/Content.jsx
+++ src/components/Header/Content.jsx
@@ -5,13 +5,13 @@ import { ToggleLanguage } from "./ToggleLanguage"
 import { ToggleTheme } from "./ToggleTheme"
 
 export function Content() {
-    const { t, i18n } = useTranslation()//TODO: Arrumar o cv para inglês
+    const { t, i18n } = useTranslation()
 
     return (
         <>
             <Link text={t('sobre')} link={"#sobre"} />
             <Link text={t('projetos')} link={"#projetos"} />
-            <Link text={"CV"} link={i18n.language === 'pt' ? "/CV - Francine Cruz-pt.pdf" : "/CV - Francine Cruz-en.pdf"} rel="noopener noreferrer" />
+            <Link text={"CV"} link={i18n.language === 'pt' ? "/CV - Francine Cruz-pt.pdf" : "/CV - Francine Cruz-en.pdf"} target="_blank" rel="noopener noreferrer" />
 
             <div className='pt-2'>
                 <ToggleTheme />
