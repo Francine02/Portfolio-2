9efd500d7d46c7fb514b845836e9d3656c975b4e
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-08-07 19:28:04 -0300

    chore: remove alias @ and @pages

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@pages': path.resolve(__dirname, './src/pages'),
       '@i18n': path.resolve(__dirname, './src/i18n'),
       '@projects': path.resolve(__dirname, './src/projects'),
     },
