6d42eb11f8d543518eda26f9d167a462ba67c23c
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-08-11 19:25:11 -0300

    refactor: sort imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 import './i18n/i18next.jsx';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
