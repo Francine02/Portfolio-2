303b342a1e32c4ceabdc20023583bfc65c1eb949
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-08-20 20:30:14 -0300

    feat (constants): added `HEADER_LINKS` with navigation and CV download links (pt/en)

---
+++ src/constants/header-links.js
@@ -0,0 +1,16 @@
+export const HEADER_LINKS = (language) => [
+  { textKey: 'header.sobre', link: '#sobre' },
+  { textKey: 'header.projetos', link: '#projetos' },
+  { textKey: 'header.experiencias', link: '#experiencias' },
+  {
+    textKey: null,
+    text: 'CV',
+    link:
+      language === 'pt'
+        ? '/CV - Francine Cruz-pt.pdf'
+        : '/CV - Francine Cruz-en.pdf',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  },
+  { textKey: 'header.contato', link: '#contato' },
+];
