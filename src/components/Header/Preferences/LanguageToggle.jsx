33bb8b2af3ab16e9447e710d482cca640134b965
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-06 21:29:20 -0300

    style: changed the padding and width measurements

--- src/components/Header/Preferences/LanguageToggle.jsx
+++ src/components/Header/Preferences/LanguageToggle.jsx
@@ -18,7 +18,7 @@ export function LanguageToggle() {
       <img
         src={i18n.language === 'pt' ? en : br}
         alt={i18n.language === 'pt' ? 'en' : 'pt'}
-        className="w-6 md:w-7 hover:opacity-70"
+        className="w-6 hover:opacity-70"
       />
       <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-max max-w-xs bg-black text-white text-sm rounded px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         {i18n.language === 'pt' ? 'English' : 'Português'}
--- src/components/Hero/ContactIcons.jsx
+++ src/components/Hero/ContactIcons.jsx
@@ -2,7 +2,7 @@ import { CONTACTS_LINKS } from '@constants/contacts-links';
 
 export function ContactIcons() {
   return (
-    <div className="grid gap-5 text-2xl sm:text-3xl w-fit absolute bottom-16 right-0">
+    <div className="grid gap-5 text-2xl sm:text-3xl w-fit absolute bottom-16 right-0 pr-7 sm:pr-12 lg:pr-24 2xl:pr-96">
       {CONTACTS_LINKS.map(({ link, icon: Icon }) => (
         <a
           key={link}
