4baa5273c06cdc41831a5e7ad730c5b511266506
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-06 21:27:18 -0300

    delete: scrollreveal of the app and components Home, About, h1, Projects, Contact and ButtonUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,14 @@
-import { useEffect } from 'react';
-import { About } from './pages/About';
-import { Home } from './pages/Home';
-import { Projects } from './pages/Projects';
-import ScrollReveal from 'scrollreveal';
-import { myProject } from './projects/myProjects.js';
-import { useTranslation } from 'react-i18next';
-import { Contact } from './pages/Contact';
-import { ButtonUp } from './components/ButtonUp/ButtonUp';
+import { AboutMe } from '@components/AboutMe/index';
+import { Header } from '@components/Header/index';
+import { Hero } from '@components/Hero/index';
 
 function App() {
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    //Para a configuração do ScrollReveal
-    const sr = ScrollReveal();
-
-    sr.reveal('.one', {
-      duration: 950,
-      origin: 'left',
-      distance: '100px',
-      easing: 'ease-in-out',
-      reset: false,
-    });
-
-    sr.reveal('.two', {
-      duration: 950,
-      origin: 'right',
-      distance: '100px',
-      easing: 'ease-in-out',
-      reset: false,
-    });
-  }, []);
-
   return (
-    <div className="dark:bg-[#272727] min-h-screen overflow-hidden">
-      <div className="one">
-        <Home />
-      </div>
-
-      <div className="two">
-        <About />
-      </div>
-
-      <h1
-        id="projetos"
-        className="pt-24 sm:pt-36 text-2xl sm:text-3xl md:text-4xl font-semibold dark:text-white text-center pb-14"
-      >
-        <span className="bg-gradient-to-r from-indigo-600 via-pink-600 to-purple-600 bg-[length:100%_3px] bg-no-repeat bg-bottom pb-1">
-          {t('projects.project')}
-        </span>
-      </h1>
-      {myProject.map((proj, index) => (
-        <Projects
-          key={proj.name}
-          name={t(proj.name)}
-          github={proj.github}
-          deploy={proj.deploy}
-          img={proj.img}
-          about={t(proj.about)}
-          tech={proj.tech}
-          reverse={index % 2 === 1} // Alterna entre true e false para cada projeto
-        />
-      ))}
-
-      <div className="two">
-        <Contact />
-      </div>
-
-      <ButtonUp />
-    </div>
+    <main className="dark:bg-[#272727] min-h-screen overflow-hidden text-text-light dark:text-text-dark ">
+      <Header />
+      <Hero />
+      <AboutMe />
+    </main>
   );
 }
 
