73230c13e31ab62562f5ad27dad35bd50e2a2cb1
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-06 21:30:14 -0300

    feat (constants): contacts-links with list of objects: github, linkedin and whatsapp

---
+++ src/constants/contacts-links.js
@@ -0,0 +1,15 @@
+import { BsWhatsapp } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
+
+export const CONTACTS_LINKS = [
+  {
+    icon: FaLinkedinIn,
+    link: 'https://www.linkedin.com/in/francine-ccruz/',
+  },
+  { icon: FiGithub, link: 'https://github.com/Francine02' },
+  {
+    icon: BsWhatsapp,
+    link: 'https://api.whatsapp.com/send/?phone=5551986283397&text&type=phone_number&app_absent=0',
+  },
+];
