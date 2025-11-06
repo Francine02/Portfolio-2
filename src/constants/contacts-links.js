0b7a9f7036b003b50eea5aa9f71eff9ff447d1ae
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-11-06 20:32:59 -0300

    delete (constants): whatsapp link

--- src/constants/contacts-links.js
+++ src/constants/contacts-links.js
@@ -1,4 +1,3 @@
-import { BsWhatsapp } from 'react-icons/bs';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { FiGithub } from 'react-icons/fi';
 
@@ -8,8 +7,4 @@ export const CONTACTS_LINKS = [
     link: 'https://www.linkedin.com/in/francine-ccruz/',
   },
   { icon: FiGithub, link: 'https://github.com/Francine02' },
-  {
-    icon: BsWhatsapp,
-    link: 'https://api.whatsapp.com/send/?phone=5551986283397&text&type=phone_number&app_absent=0',
-  },
 ];
