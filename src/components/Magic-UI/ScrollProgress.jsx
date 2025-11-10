af22010b0e7129173e0e51157238257cf2e1ee2f
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-11-10 20:18:29 -0300

    style (ScrollProgress): change color of the component

--- src/components/Magic-UI/ScrollProgress.jsx
+++ src/components/Magic-UI/ScrollProgress.jsx
@@ -8,7 +8,7 @@ export function ScrollProgress({ className, ref, ...props }) {
     <motion.div
       ref={ref}
       className={cn(
-        'fixed inset-x-0 top-0 z-50 h-px origin-left bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3]',
+        'fixed inset-x-0 top-0 z-50 h-px origin-left bg-gradient-to-r from-purple to-pink ',
         className
       )}
       style={{
