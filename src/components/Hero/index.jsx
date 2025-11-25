4026c3a7e6fc419641995502bc18d0f9b115addb
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-11-25 18:37:00 -0300

    style (components): removed repetion and used the variable created

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,18 +5,16 @@ import { Title } from './Title';
 
 export function Hero() {
   return (
-    <section className="relative h-svh w-full font-title flex flex-col justify-end items-center">
+    <section className="relative h-svh w-full font-title flex flex-col justify-end items-center [@media(max-height:500px)]:scale-80 [@media(max-height:500px)]:mt-10">
       <Title />
-
       <img
         src={avatar}
         alt="Imagem em estilo de manhwa da Francine"
-        className="max-h-[50vh] sm:max-h-[64vh] 2xl:max-h-[74vh] z-1"
+        className="max-h-[45vh] sm:max-h-[50vh] md:max-h-[60vh] z-1"
         id="avatar"
       />
-      <div className="border-1 absolute border-gray-100 dark:border-black dark:opacity-15 w-screen bottom-56 z-0" />
-      <div className="border-1 absolute border-gray-100 dark:border-black dark:opacity-15 h-svh left-10 sm:left-15 lg:left-30 2xl:left-80 z-0" />
-
+      <div className="line-hero min-w-screen bottom-56 z-0" />
+      <div className="line-hero h-svh left-10 sm:left-15 lg:left-30 2xl:left-80 z-0" />
       <ContactIcons />
     </section>
   );
