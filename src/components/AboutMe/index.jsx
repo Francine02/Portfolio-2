fcacc54cb6609b956bd4152f85fe314aa5f28fc6
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-01-16 17:10:18 -0300

    refactor(aboutMe): adjust layout structure using container

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -10,15 +10,17 @@ export function AboutMe() {
     <>
       <TitleSection title={t('sobre.titulo')} id="sobre" />
 
-      <section className="section-layout text-text-light dark:text-text-dark">
-        <div className=" md:flex md:justify-around md:flex-row flex flex-col md:gap-5 items-center md:items-start">
-          <img
-            src={avatar}
-            alt="Foto de perfil"
-            className="md:order-2 w-40 sm:w-52 lg:w-52 2xl:w-60 h-auto md:pb-1 mx-auto md:mx-0 object-contain"
-          />
+      <section className="container">
+        <div className="section-layout text-text-light dark:text-text-dark">
+          <div className=" md:flex md:justify-around md:flex-row flex flex-col md:gap-5 items-center md:items-start">
+            <img
+              src={avatar}
+              alt="Foto de perfil"
+              className="md:order-2 w-40 sm:w-52 lg:w-52 2xl:w-60 h-auto md:pb-1 mx-auto md:mx-0 object-contain"
+            />
 
-          <Content />
+            <Content />
+          </div>
         </div>
       </section>
     </>
