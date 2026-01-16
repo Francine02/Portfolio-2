2c09762318e29b05114caf0f2efb35da8aa8655a
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-01-16 17:08:30 -0300

    feat(content): add Github contribuition graph

--- src/components/AboutMe/Content.jsx
+++ src/components/AboutMe/Content.jsx
@@ -6,13 +6,19 @@ export function Content() {
   const { t } = useTranslation();
 
   return (
-    <div className="pb-11 md:py-11 md:order-1 max-w-4xl">
+    <div className="md:py-11 md:order-1 max-w-4xl">
       <div className="space-y-5 font-text">
         <p dangerouslySetInnerHTML={{ __html: t('sobre.resumo') }} />
         <p dangerouslySetInnerHTML={{ __html: t('sobre.resumo1') }} />
       </div>
 
       <Technologies />
+
+      <iframe
+        src="https://jandee.vercel.app/Francine02?footer=true&weeks=false"
+        title="GitHub contribution graph"
+        className="w-full h-24 md:h-20 lg:h-26 xl:h-40 2xl:h-44"
+      />
     </div>
   );
 }
