5606a5a20106feca89c08ab2e16749586546cdba
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-01-21 20:12:57 -0300

    feat(constants): add demo links and tech stack to projects

--- src/constants/projects-back.js
+++ src/constants/projects-back.js
@@ -1,3 +1,11 @@
+import { FaJava } from 'react-icons/fa';
+import {
+  SiFlyway,
+  SiPostgresql,
+  SiSpringboot,
+  SiSwagger,
+} from 'react-icons/si';
+
 import kandoImg from '/imgs/kando.png';
 import receitotecaImg from '/imgs/receitoteca.png';
 
@@ -6,6 +14,12 @@ const receitoteca = {
   img: receitotecaImg,
   repository: 'https://github.com/Francine02/Receitoteca',
   about: 'receitoteca',
+  techs: [
+    { name: 'Java', icon: FaJava, color: '#007396' },
+    { name: 'Spring Boot', icon: SiSpringboot, color: '#6DB33F' },
+    { name: 'PostgreSQL', icon: SiPostgresql, color: '#4169E1' },
+    { name: 'Swagger', icon: SiSwagger, color: '#85EA2D' },
+  ],
 };
 
 const kando = {
@@ -13,6 +27,13 @@ const kando = {
   img: kandoImg,
   repository: 'https://github.com/Francine02/KanDo/tree/main/backend',
   about: 'kando',
+  techs: [
+    { name: 'Java', icon: FaJava, color: '#007396' },
+    { name: 'Spring Boot', icon: SiSpringboot, color: '#6DB33F' },
+    { name: 'PostgreSQL', icon: SiPostgresql, color: '#4169E1' },
+    { name: 'Swagger', icon: SiSwagger, color: '#85EA2D' },
+    { name: 'Flyway', icon: SiFlyway, color: '#FF0000' },
+  ],
 };
 
 export const PROJECTS_BACK = [receitoteca, kando];
--- src/constants/projects-front.js
+++ src/constants/projects-front.js
@@ -1,3 +1,15 @@
+import {
+  SiAxios,
+  SiCss3,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+} from 'react-icons/si';
+
 import converImg from '/imgs/conver.png';
 import lectifyImg from '/imgs/lectify.png';
 import lectify1Img from '/imgs/lectify1.png';
@@ -9,6 +21,12 @@ const conver = {
   deploy: 'https://conver-taupe.vercel.app/',
   repository: 'https://github.com/Francine02/Conver',
   about: 'conver',
+  demo: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7257427737840078850?compact=1',
+  techs: [
+    { name: 'React', icon: SiReact, color: '#61DAFB' },
+    { name: 'Vite', icon: SiVite, color: '#7952B3' },
+    { name: 'TailwindCSS', icon: SiTailwindcss, color: '#00B9FF' },
+  ],
 };
 
 const quicklist = {
@@ -17,6 +35,12 @@ const quicklist = {
   deploy: 'https://francine02.github.io/Quicklist-Rocketseat/',
   repository: 'https://github.com/Francine02/Quicklist-Rocketseat',
   about: 'quicklist',
+  demo: '/videos/quicklist.mp4',
+  techs: [
+    { name: 'JavaScript', icon: SiJavascript, color: '#F7DF1E' },
+    { name: 'HTML', icon: SiHtml5, color: '#E34F26' },
+    { name: 'CSS', icon: SiCss3, color: '#1572B6' },
+  ],
 };
 
 const lectify1 = {
@@ -24,6 +48,14 @@ const lectify1 = {
   img: lectify1Img,
   repository: 'https://github.com/Francine02/Lectify/tree/v1',
   about: 'lectify',
+  demo: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7312509759415377921?compact=1',
+  techs: [
+    { name: 'React', icon: SiReact, color: '#61DAFB' },
+    { name: 'Next', icon: SiNextdotjs, color: '#606060' },
+    { name: 'TailwindCSS', icon: SiTailwindcss, color: '#00B9FF' },
+    { name: 'TypeScript', icon: SiTypescript, color: '#3178C6' },
+    { name: 'Axios', icon: SiAxios, color: '#00B9FF' },
+  ],
 };
 
 const lectify = {
@@ -32,6 +64,13 @@ const lectify = {
   deploy: 'https://lectify.vercel.app/',
   repository: 'https://github.com/Francine02/Lectify',
   about: 'lectify2',
+  techs: [
+    { name: 'React', icon: SiReact, color: '#61DAFB' },
+    { name: 'Next', icon: SiNextdotjs, color: '#606060' },
+    { name: 'TailwindCSS', icon: SiTailwindcss, color: '#00B9FF' },
+    { name: 'TypeScript', icon: SiTypescript, color: '#3178C6' },
+    { name: 'Axios', icon: SiAxios, color: '#00B9FF' },
+  ],
 };
 
 export const PROJECTS_FRONT = [conver, quicklist, lectify1, lectify];
