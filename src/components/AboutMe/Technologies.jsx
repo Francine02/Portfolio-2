be5a4964365f35a49e7e10924be4a4eeaf7f80b9
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-01-22 19:16:35 -0300

    refactor(AboutMe): extracts technology tooltip for reusable component

--- src/components/AboutMe/Technologies.jsx
+++ src/components/AboutMe/Technologies.jsx
@@ -1,3 +1,4 @@
+import { IconTooltip } from '@components/IconTooltip/index';
 import { TECHNOLOGIES_LIST } from '@constants/technologies-list';
 import { useTranslation } from 'react-i18next';
 
@@ -12,16 +13,8 @@ export function Technologies() {
             {category === 'Ferramentas' ? t('sobre.ferramentas') : category}:
           </h3>
           <div className="flex flex-wrap gap-3">
-            {technologies.map(({ name, icon: Icon, color }) => (
-              <div
-                key={name}
-                className="group relative flex flex-col items-center hover:scale-110 transition"
-              >
-                <Icon size={25} color={color} />
-                <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max max-w-xs bg-black text-white text-sm rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
-                  {name}
-                </span>
-              </div>
+            {technologies.map(({ name, icon, color }) => (
+              <IconTooltip key={name} name={name} icon={icon} color={color} />
             ))}
           </div>
         </div>
