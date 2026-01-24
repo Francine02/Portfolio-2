f1ee5e83f75456b77ef135160f71a1a8754d339b
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-01-24 14:49:40 -0300

    feat: add experiences, contact and footer sections to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { AboutMe } from '@components/AboutMe/index';
+import { Contact } from '@components/Contact/index';
+import { Experiences } from '@components/Experiences/index';
+import { Footer } from '@components/Footer/index';
 import { Header } from '@components/Header/index';
 import { Hero } from '@components/Hero/index';
 import { Projects } from '@components/Projects/index';
 
 function App() {
   return (
-    <main className="dark:bg-dark min-h-screen overflow-hidden text-text-light dark:text-text-dark pb-10">
+    <main className="dark:bg-dark min-h-screen overflow-hidden text-text-light dark:text-text-dark">
       <Header />
       <Hero />
 
       <AboutMe />
 
       <Projects />
+
+      <Experiences />
+
+      <Contact />
+
+      <Footer />
     </main>
   );
 }
