1771c83e0a7431a0601336276607791d734d98d8
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-02-02 18:53:17 -0300

    feat(contact): cat gif to contact section

--- src/components/Contact/Content/index.jsx
+++ src/components/Contact/Content/index.jsx
@@ -1,3 +1,4 @@
+import catGif from '@assets/cat.gif';
 import { useTranslation } from 'react-i18next';
 
 import { Form } from '../Form/index';
@@ -7,13 +8,20 @@ export function Content() {
   const { t } = useTranslation();
 
   return (
-    <div className="grid md:grid-cols-2 gap-4 lg:gap-16">
+    <div className="grid md:grid-cols-2 gap-4 lg:gap-16 ">
       <div className="mt-6">
         <h2 className="font-text lg:text-xl mb-6 ">{t('contato.obs')}</h2>
         <Links />
       </div>
 
-      <Form />
+      <div className="relative">
+        <img
+          src={catGif}
+          alt="Cat gif"
+          className="size-16 absolute -top-11 right-3 2xl:right-32"
+        />
+        <Form />
+      </div>
     </div>
   );
 }
