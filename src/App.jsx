972343a94e46762cf8c03a5f72c2169380ca6d82
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-02-10 17:57:51 -0300

    feat: add fade and scroll animations on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { fadeAnimations } from '@animations/fadeAnimations';
+import { scrollAnimations } from '@animations/scrollAnimations';
 import { AboutMe } from '@components/AboutMe/index';
 import { Contact } from '@components/Contact/index';
 import { Experiences } from '@components/Experiences/index';
@@ -5,9 +7,15 @@ import { Footer } from '@components/Footer/index';
 import { Header } from '@components/Header/index';
 import { Hero } from '@components/Hero/index';
 import { Projects } from '@components/Projects/index';
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
+  useEffect(() => {
+    fadeAnimations();
+    scrollAnimations();
+  }, []);
+
   return (
     <main className="dark:bg-dark min-h-screen overflow-hidden text-text-light dark:text-text-dark">
       <Header />
